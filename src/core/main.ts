524feb3e1c09df387cc47ac2ebfe2824da9173ce
sunya9 <mugen.xyz@gmail.com>
2020-05-07 13:07:03 +0900

    fix: remove tearDown

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,36 +1,18 @@
 import { init } from 'src/core/init'
 import { createClientInstance } from 'src/client'
 import debug from 'debug'
-import { tearDown } from 'src/core/tearDown'
-import { Context } from 'src/context'
-import WebSocket from 'ws'
 const logger = debug('pnut-stream:core:main')
 
 export async function main() {
   const { context, key } = await init()
   for (;;) {
-    const { ws, connection } = createClientInstance(context)(key)
+    const { connection } = createClientInstance(context)(key)
     try {
-      const removeListeners = setProcessEventListeners(context, ws)
       await connection
-      removeListeners()
       logger('closed, reconnecting...')
     } catch (e) {
       logger('terminated:', e)
       break
     }
-  }
-}
-
-function setProcessEventListeners(context: Context, ws: WebSocket) {
-  const shutdown = async () => {
-    logger('graceful shutdown...')
-    await tearDown(context, ws)()
-  }
-  process.on('SIGINT', shutdown)
-  process.on('SIGTERM', shutdown)
-  return () => {
-    process.off('SIGINT', shutdown)
-    process.off('SIGTERM', shutdown)
   }
 }
